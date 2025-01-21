c3b07119e2ae4dd3290b2fa55763a75e6b233248
kimgun8376 <dbasdf48287@gamil.com>
2025-01-21 17:25:57 +0900

    commit

--- HelloWorld/src/come/yedam/Variable2.java
+++ HelloWorld/src/come/yedam/Variable2.java
@@ -2,13 +2,14 @@ package come.yedam;
 
 public class Variable2 {
 	public static void main(String[] args) { // 컨트롤 스페이스 
-		for(int j=2; j<=7; j++) {
-		     int num = j;
-			for (int i =1; i <= 9; i++) {
-			//System.out.println(num + "*" + i+  "="+ num*i+"\n");
-			System.out.printf("%d *%d = %d\n", num,i, num*i); // `${num} * ${i} = ${num*i}`
+		 for(int j=1; j<=9; j++) {
+		 //    int num = j;
+			for (int i =2; i <= 6; i++) {
+			//System.out.print(num + " * " + i+  " = "+ num*i+" ");
+			System.out.printf("%d *%d = %2d ", i,j ,i*j); // `${num} * ${i} = ${num*i}`
 		    }
+			System.out.println();
 		}
-		System.out.println("end of prog");
+		System.out.println("\nend of prog");
 	}
 }
